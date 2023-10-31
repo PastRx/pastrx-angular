4f9c856c0c8804b51d1641de11516da8a9a32473
anugudivyareddy <103094857+anugudivyareddy@users.noreply.github.com>
2023-10-31 09:35:09 +0530

    implementation of  provider and user settings tab

--- src/app/administration/user-list/user-list.component.ts
+++ src/app/administration/user-list/user-list.component.ts
@@ -13,30 +13,162 @@ declare var gapi: any;
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
-  isAddUser=false;
-  isInactiveUsers=false;
-  isOnlyInactiveUsers=false;
-  isOnlyRemovedUsers=false;
-  userList: any;
-  addUserRequest(){
-   this.isAddUser=true;
+  isAddUser = false;
+  isInactiveUsers = false;
+  isOnlyInactiveUsers = false;
+  isOnlyRemovedUsers = false;
+  userListInactiveList: any; selectedItem: any;
+  userList: any; list: any; remove: any;
+  addUserRequest() {
+    this.isAddUser = true;
   }
-  stIsAddUser(evnt){
+  stIsAddUser(evnt) {
     console.log(evnt);
-    this.isAddUser=evnt;
+    this.isAddUser = evnt;
   }
-  constructor(private api: ApiService,private datePipe: DatePipe, private router: Router   ) { }
+  constructor(private api: ApiService, private datePipe: DatePipe, private router: Router) { }
+
   ngOnInit() {
+    this.getUsersList();
+  }
+
+  getUsersList() {
     this.api.getUserList(
       {
         'masquerade': PASTRX.masquerade
       }
     ).subscribe({
       next: (res) => {
-        console.log('getuserlist------------>'+res)
-        this.userList =res.items;
+        this.userList = res.items;
       },
       error: (e) => console.log(e),
     });
   }
+
+  onChange() {
+    if (this.isInactiveUsers == true) {
+      this.userList = this.userList.sort((a, b) => ((a.active < b.active) ? -1 : (a.active > b.active) ? 1 : 0));
+    }
+    else if (this.isOnlyInactiveUsers == true) {
+      this.userListInactiveList = this.userList.filter(item => item.active === false);;
+      this.userList = this.userListInactiveList;
+    }
+    else {
+      this.getUsersList();
+    }
+  }
+
+  onChangeRemove() {
+
+  }
+
+  setInactive() {
+    if (this.list != 0) {
+      this.selectedItem = [];
+      this.selectedItem.push(this.list[0].firstName, this.list[0].lastName);
+      this.api.setUserInactiveList(
+        {
+          providerList: this.selectedItem,
+          masquerade: PASTRX.masquerade
+        }).subscribe({
+          next: (response) => {
+            this.getUsersList();
+          },
+          error: (e) => console.log(e),
+        });
+    }
+  }
+
+  setActive() {
+    if (this.list != 0) {
+      this.selectedItem = [];
+      this.selectedItem.push(this.list[0].firstName, this.list[0].lastName);
+      this.api.setUserActiveList(
+        {
+          providerList: this.list,
+          masquerade: PASTRX.masquerade
+        }).subscribe({
+          next: (response) => {
+            this.getUsersList();
+          },
+          error: (e) => console.log(e),
+        });
+    }
+  }
+
+  setAdmin() {
+    this.remove = false;
+    if (this.list != 0) {
+      this.selectedItem = [];
+      this.selectedItem.push(this.list[0].id);
+      this.updateAdminList();
+    }
+  }
+
+  removeAdmin() {
+    this.remove = true;
+    if (this.list != 0) {
+      this.selectedItem = [];
+      this.selectedItem.push(this.list[0].id);
+      this.updateAdminList();
+    }
+  }
+
+  removeUsers() {
+    if (this.list != 0) {
+      this.api.setUserRemoved(
+        {
+          firstName: this.list[0].firstName,
+          lastName: this.list[0].lastName,
+          masquerade: PASTRX.masquerade,
+          email: null
+        }).subscribe({
+          next: (response) => {
+            this.getUsersList();
+          },
+          error: (e) => console.log(e),
+        });
+    }
+  }
+
+  unremoveUsers() {
+    if (this.list != 0) {
+      this.api.setUserUnremoved(
+        {
+          firstName: this.list[0].firstName,
+          lastName: this.list[0].lastName,
+          masquerade: PASTRX.masquerade,
+          email: null
+        }).subscribe({
+          next: (response) => {
+            this.getUsersList();
+          },
+          error: (e) => console.log(e),
+        });
+    }
+  }
+
+
+  getSelected(e: any, items: any) {
+    this.list = [];
+    items.selected = e.target.checked;
+    if (e.target.checked) {
+      this.list.push(items);
+    }
+  }
+
+  updateAdminList() {
+    this.api.setUserActiveList(
+      {
+        providerList: this.selectedItem,
+        masquerade: PASTRX.masquerade,
+        remove: this.remove
+      }).subscribe({
+        next: (response) => {
+          this.getUsersList();
+        },
+        error: (e) => console.log(e),
+      });
+  }
+
 }
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,9 +7,9 @@ import config from '../../auth_config.json';
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  cleanprms(parms){
+  cleanprms(parms) {
     Object.keys(parms).forEach(key => {
       if (parms[key] == null) {
         delete parms[key];
@@ -28,50 +28,82 @@ export class ApiService {
   }
   getPatientNames(): Observable<any> {
     console.log(config.apiUri);
-    const body=null;
+    const body = null;
     return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getPatientNames', body);
   }
-  listPASTEncounters(parms:any): Observable<any> {
-    console.log(parms);    
-    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/listPASTEncounters?targetDate='+ parms.targetDate, parms);
+  listPASTEncounters(parms: any): Observable<any> {
+    console.log(parms);
+    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/listPASTEncounters?targetDate=' + parms.targetDate, parms);
   }
-  getPMPData(parms:any): Observable<any> {
-    console.log(parms);  
-    var cprms = this.cleanprms(parms);  
-    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getpmpdata?'+ new URLSearchParams(cprms));
-  }
-  addUser(parms:any): Observable<any> {
-    console.log(parms);  
+  getPMPData(parms: any): Observable<any> {
+    console.log(parms);
     var cprms = this.cleanprms(parms);
-    const body=null;
-    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/addUser?'+ new URLSearchParams(cprms),body);
+    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getpmpdata?' + new URLSearchParams(cprms));
   }
-  getUserList(parms:any): Observable<any> {
-    console.log(parms);  
-    const body=null;
+  addUser(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
+    const body = null;
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/addUser?' + new URLSearchParams(cprms), body);
+  }
+  getUserList(parms: any): Observable<any> {
+    console.log(parms);
+    const body = null;
     return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getOurUsers', body);
   }
-  getUserData(parms:any): Observable<any> {
-    console.log(parms);  
-    var cprms = this.cleanprms(parms);  
+  getUserData(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
     return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getUserData?' + new URLSearchParams(cprms));
   }
 
-  getEHRIDsForUser(parms:any): Observable<any> {
-    console.log(parms);  
-    var cprms = this.cleanprms(parms);  
+  getEHRIDsForUser(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
     return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getEHRIDsForUser?' + new URLSearchParams(cprms));
   }
-  updatePractice(parms:any):Observable<any>{
+
+  setUserInactiveList(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
+    const body = null;
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/setUserInactiveList?' + new URLSearchParams(cprms), body);
+  }
+
+  setUserActiveList(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
+    const body = null;
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/setUserActiveList?' + new URLSearchParams(cprms), body);
+  }
+
+  setUserRemoved(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
+    const body = null;
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/setUserRemoved?' + new URLSearchParams(cprms), body);
+  }
+
+  setUserUnremoved(parms: any): Observable<any> {
+    console.log(parms);
+    var cprms = this.cleanprms(parms);
+    const body = null;
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/setUserUnremoved?' + new URLSearchParams(cprms), body);
+  }
+
+
+
+  updatePractice(parms: any): Observable<any> {
     console.log(parms);
     var cprms = this.cleanprms(parms);
     console.log(parms);
-    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/updatePractice?' ,cprms.practice)
+    return this.http.post('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/updatePractice?', cprms.practice)
   }
 
-  getPractice(parms:any): Observable<any>{
+  getPractice(parms: any): Observable<any> {
     console.log(parms);
-    var cprms = this.cleanprms(parms); 
-    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getPractice'+ new URLSearchParams(cprms));
+    var cprms = this.cleanprms(parms);
+    return this.http.get('https://pastrx-qa.appspot.com/_ah/api/pastAPI/v2.40/getPractice' + new URLSearchParams(cprms));
   }
+
 }
